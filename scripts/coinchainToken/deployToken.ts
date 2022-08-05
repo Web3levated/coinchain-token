15f13a7cbf8684debf68f41109014f431877000a
Louie <louis.miller@web3levated.com>
2022-08-05 12:42:02 -0700

    update readme file

--- scripts/coinchainToken/deployToken.ts
+++ scripts/coinchainToken/deployToken.ts
@@ -1,21 +1,20 @@
-import { expect } from "chai";
 import { ethers } from "hardhat";
-import UniswapRouterABI from "./abi/UniswapRouterV2.json";
+import UniswapRouterABI from "../abi/UniswapRouterV2.json";
 
 async function main(){
     const hre = require("hardhat");
     const [signer] = await ethers.getSigners();
     const routerAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"; // Uniswap Router
-    const receiverAddress = "0xf5ec8C9d97228f13c6f6B8a66FD2c4104Bf63b95"; //Gnosis safe
+    const receiverAddress = ""; //Gnosis safe
+    const tokenName = "";
+    const tokenSymbol = "";
+    const initialSupply = ethers.utils.parseEther("200000000");
 
     
     const router = new ethers.Contract(routerAddress, UniswapRouterABI, signer);
     const WETHAddress = await router.WETH();
     console.log("WETHAddress: ", WETHAddress)
 
-    const tokenName = "CCDEMO";
-    const tokenSymbol = "CCD";
-    const initialSupply = ethers.utils.parseEther("200000000");
     const coinchainToken = await (
         await ethers.getContractFactory("CoinchainToken")
     ).deploy(tokenName, tokenSymbol, initialSupply, WETHAddress, receiverAddress);
